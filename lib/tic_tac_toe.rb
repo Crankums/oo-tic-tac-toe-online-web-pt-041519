b4da0660351d7ed973d5891986fe65fff1935bc4
Craig Donavin <crdonavin@gmail.com>
2019-05-18 17:00:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,8 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
+  def turn_count
+    @board.count? {|elem| elem != " "}  
   def turn
     puts "Your move:"
     input = gets.chomp
