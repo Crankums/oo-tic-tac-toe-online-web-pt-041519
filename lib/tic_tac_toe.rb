c87091c03363f5d6299d93fd90e843498d60b03b
Craig Donavin <crdonavin@gmail.com>
2019-05-18 16:58:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,9 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
-  def turn(input)
-    input = gets
+  def turn
+    puts "Your move:"
+    input = gets.chomp
     index = input_to_index(input)
       if valid_move(index)
         move(index, play_char)
